503df15c0d1b6a6b6a43ce5eae91f775fe6dd904
giacomo <thegreat.vanderlinde@gmail.com>
2022-12-22 16:31:27 +0100

    solc version

--- contracts/rate/ChainlinkRate.sol
+++ contracts/rate/ChainlinkRate.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.17;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import { IRatePlugin } from "./IRatePlugin.sol";
--- contracts/rate/IRatePlugin.sol
+++ contracts/rate/IRatePlugin.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.17;
 
 interface IRatePlugin {
     function getLatestPrice() external view returns (int256);
