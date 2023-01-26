de31f516db1947e1cbf3597787af8f3d8dbcbc13
giacomo <thegreat.vanderlinde@gmail.com>
2023-01-26 15:39:46 +0100

    increased amount

--- contract/scripts/mintERC20.ts
+++ contract/scripts/mintERC20.ts
@@ -2,6 +2,7 @@ import hre, { ethers } from "hardhat";
 import { bn, A_DAY_IN_SECONDS, CHAINLINK_AGGREGATORS } from "../test/utils";
 import "@nomiclabs/hardhat-ethers";
 import ethernal from "hardhat-ethernal";
+import { BigNumber } from "ethers";
 
 async function main() {
   const [deployer] = await ethers.getSigners();
@@ -11,11 +12,16 @@ async function main() {
   const ERC20Mock = await ethers.getContractFactory("ERC20Mock");
   const erc20Mock = ERC20Mock.attach("0xbb3963512b9c0F28eFb4a3dcB1e0274A87d159E7");
 
-  const amount = 1e6;
-  const r = await erc20Mock.mint("0xf588c72DA4114243B3E47A0E052F28cC44303a03", amount);
-
-  console.log(`minted ${amount} to ${erc20Mock.address}`);
-  console.log("sig: ", r.hash);
+  const amount = BigNumber.from(1000).mul(BigNumber.from(10).pow(18));
+  const destWallets = ["0x02548256453D61E8Beb31013ebE8e5ddd412cBc6", "0xf588c72DA4114243B3E47A0E052F28cC44303a03"];
+  for (const destWallet of destWallets) {
+    console.log("amount: " + amount);
+    console.log("mint addr: " + erc20Mock.address);
+    console.log("beneficiary: " + destWallet);
+    const r = await erc20Mock.mint(destWallet, amount);
+    console.log("sig: ", r.hash);
+    console.log();
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
