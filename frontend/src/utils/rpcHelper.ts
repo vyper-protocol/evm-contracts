f8bf830745b63967385ca0233a6ac3e9e3011af2
giacomo <thegreat.vanderlinde@gmail.com>
2023-01-26 16:11:42 +0100

    included websocket

--- frontend/src/utils/rpcHelper.ts
+++ frontend/src/utils/rpcHelper.ts
@@ -1,14 +1,23 @@
-export const rpc = (chain) => {
+export const rpc = (
+  chain
+): {
+  http: string;
+  webSocket?: string | undefined;
+} | null => {
   let http = "";
+  let webSocket = "";
   switch (chain.id) {
     case 97:
       http = process.env.REACT_APP_RPC_BSC_TESTNET!;
+      webSocket = process.env.REACT_APP_RPC_WS_BSC_TESTNET!;
       break;
     case 5:
       http = process.env.REACT_APP_RPC_GOERLI!;
+      webSocket = process.env.REACT_APP_RPC_WS_GOERLI!;
       break;
   }
   return {
     http,
+    webSocket,
   };
 };
