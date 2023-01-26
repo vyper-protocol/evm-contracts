9c4fde207e82e52ada36648786165eb36e57b242
giacomo <thegreat.vanderlinde@gmail.com>
2023-01-26 16:11:32 +0100

    initialized selectedChainID to 97

--- frontend/src/store/useAppStore.ts
+++ frontend/src/store/useAppStore.ts
@@ -6,19 +6,7 @@ interface AppStore {
   setSelectedChainID: (v: number) => void;
 }
 
-// // Log every time state is changed
-// const log = (config: StateCreator<AppStore, [], [], AppStore>) => (set, get, api) =>
-//   config(
-//     (...args: any) => {
-//       console.log("  applying", args);
-//       set(...args);
-//       console.log("  new state", get());
-//     },
-//     get,
-//     api
-//   );
-
 export const useAppStore = create<AppStore>()((set) => ({
-  selectedChainID: 0,
+  selectedChainID: 97,
   setSelectedChainID: (v: number) => set(() => ({ selectedChainID: v })),
 }));
